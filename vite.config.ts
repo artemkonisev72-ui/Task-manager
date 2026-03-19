0475d295a366dbdc064e73c6f2ba8245bc6c17b0
Artemkonisev72-ui <Artemkonisev72@gmail.com>
2026-03-19 22:52:20 +0500

    https

--- vite.config.ts
+++ vite.config.ts
@@ -32,15 +32,16 @@ export default defineConfig({
 		})
 	],
 	server: {
-        host: '0.0.0.0',     // слушать все интерфейсы (внешний IP будет виден)
-        port: 4173,          // твой желаемый порт, например 3000
-        strictPort: false,   // если порт занят, попробовать следующий
-        allowedHosts: [
+		host: '0.0.0.0',     // слушать все интерфейсы (внешний IP будет виден)
+		port: 4173,          // твой желаемый порт, например 3000
+		strictPort: false,   // если порт занят, попробовать следующий
+		https: true,
+		allowedHosts: [
 			'whoops.oops.wtf',
 			'oops.wtf',
-            '0.0.0.0',      // или 'your_ip_here', или true
-            'localhost',
-            '127.0.0.1'
-        ]
-    }
+			'0.0.0.0',      // или 'your_ip_here', или true
+			'localhost',
+			'127.0.0.1'
+		]
+	}
 });
