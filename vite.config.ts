2b9efb892edba7d3c8434abc6ef2b24fd3404dd3
Artemkonisev72-ui <Artemkonisev72@gmail.com>
2026-03-19 23:56:17 +0500

    добавил недостающие иконки, исправил сервис воркеров

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
 		sveltekit(),
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+				type: 'module'
+			},
 			manifest: {
 				name: 'Task Manager PWA',
 				short_name: 'TaskManager',
@@ -17,16 +21,24 @@ export default defineConfig({
 				theme_color: '#000000',
 				background_color: '#fafafa',
 				display: 'standalone',
+				start_url: '/',
+				scope: '/',
 				icons: [
 					{
-						src: 'favicon.png',
+						src: 'pwa-192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: 'favicon.png',
+						src: 'pwa-512.png',
 						sizes: '512x512',
 						type: 'image/png'
+					},
+					{
+						src: 'pwa-512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
 					}
 				]
 			}
