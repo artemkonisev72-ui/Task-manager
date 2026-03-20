2440209708519fc39048d3a64c3843a8929c6adc
artem <artemkonisev72@gmail.com>
2026-03-20 07:49:52 +0300

    Иван сделал белый хттпс

--- vite.config.ts
+++ vite.config.ts
@@ -43,21 +43,21 @@ export default defineConfig({
 				]
 			}
 		}),
-		basicSsl({
-			domains: ['whoops.oops.wtf', 'localhost', '127.0.0.1']
-		})
+
+		// basicSsl({
+		// 	domains: ['whoops.oops.wtf', 'localhost', '127.0.0.1']
+		// })
 	],
+	define: {'import.meta.env.VITE_PUBLIC_BASE_URL': JSON.stringify('https://whoops.oops.wtf'),},
 	server: {
 		host: '0.0.0.0',     // слушать все интерфейсы (внешний IP будет виден)
 		port: 4173,          // твой желаемый порт, например 3000
-		strictPort: false,   // если порт занят, попробовать следующий
-		https: true,
+		strictPort: true,   // если порт занят, попробовать следующий
 		allowedHosts: [
 			'whoops.oops.wtf',
 			'oops.wtf',
 			'0.0.0.0',      // или 'your_ip_here', или true
-			'localhost',
-			'127.0.0.1'
+			'localhost'
 		]
 	}
 });
